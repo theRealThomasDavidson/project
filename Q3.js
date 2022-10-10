b158b045ab7c5b02c4c6396cd352448ae320a371
theRealThomasDavidson <davidson.thomasj@gmail.com>
2022-10-10 17:02:20 -0400

    Q3 first pass

---
+++ Q3.js
@@ -0,0 +1,52 @@
+function drawTriangle() {
+    const canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+      const ctx = canvas.getContext('2d');
+  
+      ctx.beginPath();
+      ctx.moveTo(160, 50);
+      ctx.lineTo(190, 75);
+      ctx.lineTo(230, 25);
+      ctx.fill();
+    }
+  }
+
+  function drawQuadralateral() {
+    const canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+      const ctx = canvas.getContext('2d');
+  
+      ctx.beginPath();
+      ctx.moveTo(20, 50);
+      ctx.lineTo(32, 32);
+      ctx.lineTo(100, 15);
+      ctx.lineTo(140, 60);
+      
+      ctx.fill();
+    }
+  }
+
+  
+  function drawPentagon() {
+    const canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+      const ctx = canvas.getContext('2d');
+  
+      ctx.beginPath();
+      ctx.moveTo(274, 10);
+      ctx.lineTo(350, 32);
+      ctx.lineTo(300, 90);
+      ctx.lineTo(240, 60);
+      ctx.lineTo(240,32);
+      
+      ctx.fill();
+    }
+  }
+
+  function clearBox() {
+    const canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+      const ctx = canvas.getContext('2d');
+      ctx.clearRect(0,0,canvas.clientWidth, canvas.clientHeight)
+    }
+  }
