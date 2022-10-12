fe5b660c751ab306349ffe4af04b62bd3e4a6269
theRealThomasDavidson <davidson.thomasj@gmail.com>
2022-10-12 16:00:35 -0400

    ready for demo

--- Q4.js
+++ Q4.js
@@ -1,37 +1,44 @@
 //load from file 
-var fileName = 'Q4.json';
-var lnum = 0;
-
+var fileName = "data.json"
+var json;
 var objlist = []
-var mydata = JSON.parse(data);
-console.log(mydata[0]);
-
+var lnum = 0
+window.onload= function(){
+    document.getElementById("input_file")
+    .addEventListener('change',function (){
+        var fr = new FileReader();
+        fr.onload=function() {
+            json = JSON.parse(fr.result)
+            console.log("asdf")
+        }
+    })
+}
 
 function addTeam(data){
+    document.getElementById("input_file").remove
+    console.log(objlist)
     var contain = document.createElement("div");
     const element = document.getElementById("container");
     element.appendChild(contain);
     var line1 = document.createElement("p");
     var line2 = document.createElement("p");
-    var node = document.createTextNode("For person:"+ fn +" " + ln);
+    var node = document.createTextNode("For person:"+ data.first_name +" " + data.last_name);
     line1.appendChild(node);
-    node = document.createTextNode("id: "+pid+"  Phone Number: " + pn);
+    node = document.createTextNode("id: "+data.id+"  Phone Number: " + data.phone_number);
     line2.appendChild(node);
     contain.appendChild(line1);
     contain.appendChild(line2);
-    console.log()
     lnum += 2
     objlist.push(data);
 }
 
 function saveJSON(){
     data = objlist;
+    console.log(objlist)
     if(!data) {
         console.error('No data')
         return;
     }
-
-    filename = fileName
 
     if(typeof data === "object"){
         data = JSON.stringify(data, undefined, 4)
@@ -41,7 +48,7 @@ function saveJSON(){
         e    = document.createEvent('MouseEvents'),
         a    = document.createElement('a')
 
-    a.download = filename
+    a.download = "data.json"
     a.href = window.URL.createObjectURL(blob)
     a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
     e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
@@ -55,6 +62,5 @@ function process() {
     let pn = document.getElementById('phone_number').value;
     var data = {id:pid, first_name:fn, last_name:ln, phone_number:pn};
     addTeam(data)
-    console.log(data)
 }
 
